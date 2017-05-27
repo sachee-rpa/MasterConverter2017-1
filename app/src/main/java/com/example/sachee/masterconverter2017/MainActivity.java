c869863ed063d51d578c9473aeffd0d936b05463
sachee-rpa <sachee999@gmail.com>
2017-05-27 09:04:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sachee/masterconverter2017/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sachee.masterconverter2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUplord(){
+        int i =1;
+         i = i+1;
+
+    }
+}
